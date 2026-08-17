59a007800c7c23db070f78cc594ce3781da1e24c
Tursunpolat Iskandarov <tursunpolat@example.com>
2026-08-17 19:06:56 +0500

    Add Yandex Metrika integration

--- frontend/src/components/Analytics.tsx
+++ frontend/src/components/Analytics.tsx
@@ -2,6 +2,7 @@ import Script from "next/script";
 
 export default function Analytics() {
   const GA_MEASUREMENT_ID = "G-X1M94ZDG62";
+  const YM_COUNTER_ID = "111689384";
 
   return (
     <>
@@ -24,6 +25,33 @@ export default function Analytics() {
           `,
         }}
       />
+
+      {/* Yandex Metrika */}
+      <Script
+        id="yandex-metrika"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            m[i].l=1*new Date();
+            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+            (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+         
+            ym(${YM_COUNTER_ID}, "init", {
+                 clickmap:true,
+                 trackLinks:true,
+                 accurateTrackBounce:true,
+                 webvisor:true
+            });
+          `,
+        }}
+      />
+      <noscript>
+        <div>
+          <img src={`https://mc.yandex.ru/watch/${YM_COUNTER_ID}`} style={{ position: "absolute", left: "-9999px" }} alt="" />
+        </div>
+      </noscript>
     </>
   );
 }
